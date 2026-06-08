a6d04cf08aefc806da77a55d3e0b37681d455b1c
Dolev888 <dolev007001@gmail.com>
2026-06-08 09:51:06 +0300

    .

--- Endless-Runner-Demo/Assets/Scripts/Player.cs
+++ Endless-Runner-Demo/Assets/Scripts/Player.cs
@@ -32,8 +32,6 @@ public class Player : MonoBehaviour
     private Vector2 rollingOffset;
     private Vector2 jumpingOffset = new Vector2(-0.02f, 0.57f);
     private Vector2 jumpingSize = new Vector2(0.61f, 0.74f);
-    private float _groundCheckSizeOnGround;
-    private float _groundCheckSizeinEir= -1.07f;
 
     [Header("Rolling")]
     [SerializeField] private float rollDuration = .6f;
@@ -53,8 +51,7 @@ public class Player : MonoBehaviour
     public int health = 3;
 
     [Header("debug")]
-    public bool _HpOn;
-
+    [SerializeField] private bool _hpOn;
 
     private void Awake()
     {
@@ -66,7 +63,6 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        _groundCheckSizeOnGround = _groundCheckDistance;
         SetColliders();
     }
 
@@ -123,13 +119,12 @@ public class Player : MonoBehaviour
         jumpRemaining -= 1;
         if (jumpRemaining == 0)
             isDoubleJumping = true;
-        
+
         float jumpingForce = jumpRemaining == 1 ? firstJumpForce : doubleJumpForce;
         _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, 0f);
         _rigidbody.AddForce(Vector2.up * jumpingForce, ForceMode2D.Impulse);
         _boxCollider.offset = jumpingOffset;
         _boxCollider.size = jumpingSize;
-        _groundCheckDistance = _groundCheckSizeinEir;
     }
 
 
@@ -199,7 +194,6 @@ public class Player : MonoBehaviour
             _boxCollider.size = standingSize;
             _boxCollider.offset = standingOffset;
         }
-
     }
 
     private void SetColliders()
@@ -222,7 +216,6 @@ public class Player : MonoBehaviour
             isGrounded = true;
             isDoubleJumping = false;
             jumpRemaining = 2;
-            _groundCheckDistance = _groundCheckSizeOnGround;
             if (!isRolling)
             {
                 _boxCollider.offset = standingOffset;
@@ -242,7 +235,7 @@ public class Player : MonoBehaviour
         {
             Debug.Log(collision.gameObject.GetComponentInParent<Obstacle>()._passPoint);
             RunGameManeger.Instance.InvokeClearOnScreenObstacles();
-            if (health >= 1 && _HpOn)
+            if (health >= 1 && _hpOn)
             {
                 health -= 1;
                 OnPlayerHit?.Invoke(health);
