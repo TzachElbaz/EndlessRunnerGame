9396a06c1f2f0f04a328a7b0a09d3f75bb0457d1
Tsah Elbaz <tsah.elbaz@gmail.com>
2026-06-11 21:32:06 +0300

    tutorial platform before code

---
+++ Endless-Runner-Demo/Assets/Scripts/Platform.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    Player player;
+    [SerializeField] private float depth = 1;
+    [SerializeField] private float destroy = -30;
+    [SerializeField] private float spawn = 75;
+
+    private void Awake()
+    {
+        player = GameObject.FindAnyObjectByType<Player>();
+    }
+
+    private void Update()
+    {
+        float realVelocity = player.velocity.x / depth;
+        Vector2 position = transform.position;
+
+        position.x -= realVelocity * Time.deltaTime;
+
+        if (position.x < destroy)
+            position.x = spawn;
+
+        transform.position = position;
+    }
+
+}
